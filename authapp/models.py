3886d77d53fe3311c6ff8eeafb2897d1626d52ad
976Evil <an.lebedev@mail.ru>
2021-08-04 23:42:37 +0300

    fin

--- authapp/models.py
+++ authapp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from datetime import datetime, date, time,timedelta
 import django.conf.global_settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from hypothesis.extra import pytz
 
@@ -18,3 +20,25 @@ class ShopUser(AbstractUser):
         if datetime.now(pytz.timezone(settings.TIME_ZONE)) < (self.activation_key_created + timedelta(hours=48)):
             return False
         return True
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+    GENDER_CHOICES = (
+        (MALE,'М'),
+        (FEMALE,'Ж'),
+    )
+    user = models.OneToOneField(ShopUser,on_delete=models.CASCADE,unique=True,db_index=True)
+    tagline = models.CharField(max_length=150,blank = True,verbose_name='теги')
+    about_me = models.TextField(blank = True,verbose_name='о себе')
+    gender = models.CharField(choices=GENDER_CHOICES,blank=True,max_length=1,verbose_name='пол')
+
+
+    @receiver(post_save,sender=ShopUser)
+    def create_user_profile(sender,instance,created,**kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance,**kwargs):
+        instance.shopuserprofile.save()
+
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xxac+ln+74ue7w1tj7%l3kw)9ygh893s6q$g)5ps4c#s!=19yk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
@@ -67,12 +68,15 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'mainapp.context_processors.basket'
+                'mainapp.context_processors.basket',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
 ]
 
+LOGIN_URL = '/'
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
 
@@ -155,8 +159,23 @@ AUTHENTICATION_BACKENDS = (
 
 )
 
-SOCIAL_AUTH_VK_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_VK_OAUTH2_KEY')
+SOCIAL_AUTH_VK_OAUTH2_KEY = "7917562"
 SOCIAL_AUTH_VK_OAUTH2_SECRET = "Of1TFzSCMv2BKbG7XgbG"
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+SOCIAL_AUTH_VK_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.create_user',
+    'authapp.pipeline.save_user_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
+# DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
